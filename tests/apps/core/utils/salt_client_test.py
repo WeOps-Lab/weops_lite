8a7ea99815fa0add7f8f7fb0b59ddac9f423fcfa
umaru <15875339926@139.com>
2024-01-11 15:08:54 +0800

    feature: 重构SaltClient类并添加测试用例
    - 删除未使用的`requests`导入语句
    - 在`SaltClient`类初始化中添加日志记录
    - 更新SaltAPI身份验证失败的错误信息
    - 添加一个私有方法`__execute`来处理API执行
    - 添加方法来执行不同类型的Salt命令
    - 添加一个测试用例来初始化SaltClient并记录成功的令牌获取
    - 添加一个测试用例来执行`execute_salt_ssh`方法并记录结果
    - 添加一个测试用例来执行`execute_salt_local`方法并记录结果

--- tests/apps/core/utils/salt_client_test.py
+++ tests/apps/core/utils/salt_client_test.py
@@ -1,9 +1,23 @@
+import logging
+
 from apps.core.utils.salt_client import SaltClient
 
 
 class TestSaltClient:
     def setup_method(self):
+        self.logger = logging.getLogger(__name__)
         self.client = SaltClient()
 
-    def test_client(self):
-        print(self.client.headers)
+    def test_init_client(self):
+        self.logger.info(f'获取Token成功:{self.client.headers}')
+
+    def test_execute_salt_ssh(self):
+        result = self.client.execute_salt_ssh('cmd.run', '*', 'ps -ef')
+        self.logger.info(f'任务执行结果为:{result}')
+
+    def test_execute_salt_local(self):
+        result = self.client.execute_salt_local('test.ping', '*', '')
+        self.logger.info(f'任务执行结果为:{result}')
+
+        result = self.client.execute_salt_local('cmd.run', '*', 'df -h')
+        self.logger.info(f'任务执行结果为:{result}')
