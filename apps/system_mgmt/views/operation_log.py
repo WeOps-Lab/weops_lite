bef1e75241b1f3e3957a7d2eabe4d64315ea5891
umaru <15875339926@139.com>
2024-01-30 16:11:50 +0800

    minor: rename permission and modify list function for operation log
    - Rename the permission `"OperationLog_list"` to `"operation_log_list"`
    - Modify the `list` function to return a success response

--- apps/system_mgmt/views/operation_log.py
+++ apps/system_mgmt/views/operation_log.py
@@ -3,6 +3,7 @@ from rest_framework.viewsets import GenericViewSet
 
 from apps.core.decorators.uma_permission import uma_permission
 from apps.core.utils.drf_utils import CustomPageNumberPagination
+from apps.core.utils.web_utils import WebUtils
 from apps.system_mgmt.filters.operation_log import OperationLogFilter
 from apps.system_mgmt.models.operation_log import OperationLog
 from apps.system_mgmt.serializers.operation_log import OperationLogSer
@@ -16,6 +17,7 @@ class OperationLogViewSet(ListModelMixin, GenericViewSet):
     filter_class = OperationLogFilter
     pagination_class = CustomPageNumberPagination
 
-    @uma_permission("OperationLog_list")
+    @uma_permission("operation_log_list")
     def list(self, request, *args, **kwargs):
-        return super().list(request, *args, **kwargs)
+        results = super().list(request, *args, **kwargs)
+        return WebUtils.response_success(results.data)
