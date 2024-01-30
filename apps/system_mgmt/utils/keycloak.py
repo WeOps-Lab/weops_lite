40986ef7e6a03f116e12086f3a5e880a045448a2
baiyf <baiyf@canway.net>
2024-01-30 19:34:02 +0800

    feature-过滤初始内置角色

--- apps/system_mgmt/utils/keycloak.py
+++ apps/system_mgmt/utils/keycloak.py
@@ -1,6 +1,7 @@
 from keycloak import urls_patterns, KeycloakGetError
 from keycloak.exceptions import raise_error_from_response
 
+from apps.system_mgmt.constants import BUILT_IN_ROLES
 from weops_lite.components.keycloak import KEYCLOAK_CLIENT_ID
 
 
@@ -19,6 +20,18 @@ def get_client_id(realm_client):
     for client in clients:
         if client["clientId"] == KEYCLOAK_CLIENT_ID:
             return client["id"]
+
+
+def get_realm_roles(realm_client):
+    """获取域角色，并过滤掉内置角色"""
+    result = realm_client.get_realm_roles()
+    return [i for i in result if i.get("name") not in BUILT_IN_ROLES]
+
+
+def get_realm_roles_of_user(realm_client, user_id):
+    """获取用户关联的角色，并过滤掉内置角色"""
+    result = realm_client.get_realm_roles_of_user(user_id)
+    return [i for i in result if i.get("name") not in BUILT_IN_ROLES]
 
 
 # python-keycloak库缺少的api, 在此处补充
