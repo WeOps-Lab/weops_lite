8e571b1d76f879034736cbfd30a2770ed0fa7ed2
baiyf <baiyf@canway.net>
2024-02-04 19:04:12 +0800

    feature-补充用户自身增删改查测试场景

--- apps/system_mgmt/features/steps/用户.py
+++ apps/system_mgmt/features/steps/用户.py
@@ -1,14 +1,95 @@
 from behave import given, when, then
-from dotenv import load_dotenv
+from apps.core.utils.keycloak_client import KeyCloakClient
 
 from apps.core.features.utils.mock_request_client import MockRequestClient
 
 
 @when('用户点击获取自己的用户信息')
 def 用户点击获取自己的用户信息(context):
-    context.user_info = MockRequestClient().get('api/login_info/')
+    resp = MockRequestClient().get('api/login_info/')
+    context.user_info = resp["data"]["user_info"]
 
 
-@then('用户的名称是 {username}')
+@then('用户的名称是{username}')
 def 用户的名称是_username(context, username):
-    assert context.user_info['data']['user_info']['name'] == username
+    assert context.user_info['name'] == username
+
+
+@given("假设用户信息如下，用户名：{username}，密码：{password}，邮箱：{email}，中文名：{lastName}")
+def 设置用户信息(context, username, password, email, lastName):
+    context.username = username
+    context.password = password
+    context.email = email
+    context.lastName = lastName
+
+
+@when('根据用户信息创建用户，并记录用户ID')
+def 创建用户(context):
+    data = dict(
+        username=context.username,
+        password=context.password,
+        email=context.email,
+        lastName=context.lastName,
+    )
+    resp = MockRequestClient().post('api/user/', data)
+    context.user_id = resp["data"]["id"]
+
+
+@then('根据用户ID，查询用户信息并进行信息校验')
+def 查询用户信息(context):
+    resp = MockRequestClient().get(f'api/user/{context.user_id}/')
+
+    assert context.username == resp["data"]["username"]
+    assert context.email == resp["data"]["email"]
+    assert context.lastName == resp["data"]["lastName"]
+
+
+@given("查找用户名为{username}的用户信息，并记录")
+def 查找并设置用户信息(context, username):
+    resp = MockRequestClient().get(f"api/user/?search={username}")
+    for user_info in resp["data"]["users"]:
+        if user_info["username"] == username:
+            context.user_info = user_info
+            break
+
+
+@when('修改用户中文名称为：{new_lastName}，邮箱为：{new_email}')
+def 修改用户信息(context, new_lastName, new_email):
+    data = dict(
+        email=new_email,
+        lastName=new_lastName,
+    )
+    MockRequestClient().put(f'api/user/{context.user_info["id"]}/', data)
+    context.user_id = context.user_info["id"]
+    context.username = context.user_info["username"]
+    context.email = new_email
+    context.lastName = new_lastName
+
+
+@when('重置用户密码为{new_password}')
+def 重置用户密码(context, new_password):
+    context.new_password = new_password
+    MockRequestClient().patch(f'api/user/{context.user_info["id"]}/', dict(password=new_password))
+
+
+@then('用户登录，用户名：{username}，密码：{password}')
+def 验证用户登录(context, username, password):
+    resp = KeyCloakClient().get_token(username, password)
+    assert resp.success
+
+
+@when('点击删除用户')
+def 删除用户(context):
+    MockRequestClient().delete(f'api/user/{context.user_info["id"]}/')
+
+
+@then('不存在用户：{username}')
+def 验证用户登录(context, username):
+    resp = MockRequestClient().get(f"api/user/?search={username}/")
+    absent_username = True
+    for user_info in resp["data"]["users"]:
+        if user_info["username"] == username:
+            absent_username = False
+            break
+    assert absent_username
+
--- apps/system_mgmt/features/用户.feature
+++ apps/system_mgmt/features/用户.feature
@@ -2,4 +2,24 @@ Feature: 用户
 
   Scenario: 获取用户信息
     When 用户点击获取自己的用户信息
-    Then 用户的名称是 管理员
+    Then 用户的名称是超管
+
+  Scenario: 创建用户
+    Given 假设用户信息如下，用户名：test，密码：test123，邮箱：test@qq.com，中文名：测试员
+    When 根据用户信息创建用户，并记录用户ID
+    Then 根据用户ID，查询用户信息并进行信息校验
+
+  Scenario: 修改用户信息
+    Given 查找用户名为test的用户信息，并记录
+    When 修改用户中文名称为：测试人员，邮箱为：test@163.com
+    Then 根据用户ID，查询用户信息并进行信息校验
+
+  Scenario: 重置用户密码
+    Given 查找用户名为test的用户信息，并记录
+    When 重置用户密码为test321
+    Then 用户登录，用户名：test，密码：test321
+
+  Scenario: 删除用户
+    Given 查找用户名为test的用户信息，并记录
+    When 点击删除用户
+    Then 不存在用户：test
