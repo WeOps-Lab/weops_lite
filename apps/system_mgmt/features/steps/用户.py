7d20e7bbddba5517fe3b6e042d13e53dfe0e9f97
baiyf <baiyf@canway.net>
2024-02-05 17:34:18 +0800

    feature-添加用户管理场景测试

--- apps/system_mgmt/features/steps/用户.py
+++ apps/system_mgmt/features/steps/用户.py
@@ -1,3 +1,5 @@
+import json
+
 from behave import given, when, then
 from apps.core.utils.keycloak_client import KeyCloakClient
 
@@ -15,33 +17,31 @@ def 用户的名称是_username(context, username):
     assert context.user_info['name'] == username
 
 
-@given("假设用户信息如下，用户名：{username}，密码：{password}，邮箱：{email}，中文名：{lastName}")
-def 设置用户信息(context, username, password, email, lastName):
-    context.username = username
-    context.password = password
-    context.email = email
-    context.lastName = lastName
+@given("假设用户信息如下，用户信息：{userinfo}")
+def 设置用户信息(context, userinfo):
+    context.userinfo = json.loads(userinfo)
 
 
 @when('根据用户信息创建用户，并记录用户ID')
 def 创建用户(context):
-    data = dict(
-        username=context.username,
-        password=context.password,
-        email=context.email,
-        lastName=context.lastName,
-    )
-    resp = MockRequestClient().post('api/user/', data)
+    resp = MockRequestClient().post('api/user/', context.userinfo)
     context.user_id = resp["data"]["id"]
 
 
 @then('根据用户ID，查询用户信息并进行信息校验')
 def 查询用户信息(context):
     resp = MockRequestClient().get(f'api/user/{context.user_id}/')
+    new_email = context.new_userinfo.get("email", context.userinfo["email"])
+    new_lastName = context.new_userinfo.get("lastName", context.userinfo["lastName"])
 
-    assert context.username == resp["data"]["username"]
-    assert context.email == resp["data"]["email"]
-    assert context.lastName == resp["data"]["lastName"]
+    assert context.userinfo["username"] == resp["data"]["username"]
+    assert new_email == resp["data"]["email"]
+    assert new_lastName == resp["data"]["lastName"]
+
+
+@then('点击删除用户')
+def 删除用户(context):
+    MockRequestClient().delete(f'api/user/{context.user_id}/')
 
 
 @given("查找用户名为{username}的用户信息，并记录")
@@ -53,38 +53,40 @@ def 查找并设置用户信息(context, username):
             break
 
 
-@when('修改用户中文名称为：{new_lastName}，邮箱为：{new_email}')
-def 修改用户信息(context, new_lastName, new_email):
-    data = dict(
-        email=new_email,
-        lastName=new_lastName,
-    )
-    MockRequestClient().put(f'api/user/{context.user_info["id"]}/', data)
-    context.user_id = context.user_info["id"]
-    context.username = context.user_info["username"]
-    context.email = new_email
-    context.lastName = new_lastName
+@when('修改用户为：{new_userinfo}')
+def 修改用户信息(context, new_userinfo):
+    new_userinfo = json.loads(new_userinfo)
+    data = {}
+    if "email" in new_userinfo:
+        data.update(email=new_userinfo["email"])
+    if "lastName" in new_userinfo:
+        data.update(lastName=new_userinfo["lastName"])
+
+    MockRequestClient().put(f'api/user/{context.user_id}/', data)
+    context.new_userinfo = new_userinfo
 
 
 @when('重置用户密码为{new_password}')
 def 重置用户密码(context, new_password):
     context.new_password = new_password
-    MockRequestClient().patch(f'api/user/{context.user_info["id"]}/', dict(password=new_password))
+    MockRequestClient().patch(f'api/user/{context.user_id}/', dict(password=new_password))
 
 
-@then('用户登录，用户名：{username}，密码：{password}')
-def 验证用户登录(context, username, password):
+@then('用户登录')
+def 验证用户登录(context):
+    username, password = context.userinfo["username"], context.new_password
     resp = KeyCloakClient().get_token(username, password)
     assert resp.success
 
 
 @when('点击删除用户')
 def 删除用户(context):
-    MockRequestClient().delete(f'api/user/{context.user_info["id"]}/')
+    MockRequestClient().delete(f'api/user/{context.user_id}/')
 
 
-@then('不存在用户：{username}')
-def 验证用户登录(context, username):
+@then('不存在用户')
+def 验证用户登录(context):
+    username = context.userinfo["username"]
     resp = MockRequestClient().get(f"api/user/?search={username}/")
     absent_username = True
     for user_info in resp["data"]["users"]:
--- apps/system_mgmt/features/用户.feature
+++ apps/system_mgmt/features/用户.feature
@@ -4,22 +4,49 @@ Feature: 用户
     When 用户点击获取自己的用户信息
     Then 用户的名称是超管
 
-  Scenario: 创建用户
-    Given 假设用户信息如下，用户名：test，密码：test123，邮箱：test@qq.com，中文名：测试员
+  Scenario Outline: 创建用户
+    Given 假设用户信息如下，用户信息：<用户信息>
     When 根据用户信息创建用户，并记录用户ID
     Then 根据用户ID，查询用户信息并进行信息校验
+    And 点击删除用户
+    Examples:
+      |用户信息|
+      |{"username":"test1", "password":"pwd1", "email":"test1@163.com", "lastName":"测试1"}|
+      |{"username":"test2", "password":"pwd2", "email":"test2@163.com", "lastName":"测试2"}|
+      |{"username":"test3", "password":"pwd3", "email":"test3@163.com", "lastName":"测试3"}|
 
-  Scenario: 修改用户信息
-    Given 查找用户名为test的用户信息，并记录
-    When 修改用户中文名称为：测试人员，邮箱为：test@163.com
+  Scenario Outline: 修改用户信息(只支持修改email、lastName)
+    Given 假设用户信息如下，用户信息：<用户信息>
+    When 根据用户信息创建用户，并记录用户ID
+    And 修改用户为：<要修改的用户信息>
     Then 根据用户ID，查询用户信息并进行信息校验
+    And 点击删除用户
+    Examples:
+      |用户信息|要修改的用户信息|
+      |{"username":"test4", "password":"pwd4", "email":"test4@163.com", "lastName":"测试4"}|{"email":"test44@163.com"}|
+      |{"username":"test5", "password":"pwd5", "email":"test5@163.com", "lastName":"测试5"}|{"lastName":"测试55"}|
+      |{"username":"test6", "password":"pwd6", "email":"test6@163.com", "lastName":"测试6"}|{"email":"test66@163.com", "lastName":"测试66"}|
 
-  Scenario: 重置用户密码
-    Given 查找用户名为test的用户信息，并记录
-    When 重置用户密码为test321
-    Then 用户登录，用户名：test，密码：test321
+  Scenario Outline: 重置用户密码
+    Given 假设用户信息如下，用户信息：<用户信息>
+    When 根据用户信息创建用户，并记录用户ID
+    And 重置用户密码为<新密码>
+    Then 用户登录
+    And 点击删除用户
+    Examples:
+      |用户信息|新密码|
+      |{"username":"test7", "password":"pwd7", "email":"test7@163.com", "lastName":"测试7"}|pwd77|
+      |{"username":"test8", "password":"pwd8", "email":"test8@163.com", "lastName":"测试8"}|pwd88|
+      |{"username":"test9", "password":"pwd9", "email":"test9@163.com", "lastName":"测试9"}|pwd99|
 
-  Scenario: 删除用户
-    Given 查找用户名为test的用户信息，并记录
-    When 点击删除用户
-    Then 不存在用户：test
+
+  Scenario Outline: 删除用户
+    Given 假设用户信息如下，用户信息：<用户信息>
+    When 根据用户信息创建用户，并记录用户ID
+    And 点击删除用户
+    Then 不存在用户
+    Examples:
+      |用户信息|
+      |{"username":"test10", "password":"pwd10", "email":"test10@163.com", "lastName":"测试10"}|
+      |{"username":"test11", "password":"pwd11", "email":"test11@163.com", "lastName":"测试11"}|
+      |{"username":"test12", "password":"pwd12", "email":"test12@163.com", "lastName":"测试12"}|
