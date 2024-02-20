b131a1c1059f9e224bd1f02fb64477d6c2646be9
baiyf <baiyf@canway.net>
2024-02-20 11:32:57 +0800

    feature-新增操作日志操作类型枚举值查询接口

--- apps/system_mgmt/urls.py
+++ apps/system_mgmt/urls.py
@@ -5,7 +5,7 @@ from apps.system_mgmt.views.log_entry_viewset import LogEntryViewSet
 from apps.system_mgmt.views.login_info import LoginInfoView
 from apps.system_mgmt.views.logo import LogoViewSet
 from apps.system_mgmt.views.menu_manage import MenuManageModelViewSet
-from apps.system_mgmt.views.operation_log import OperationLogViewSet
+from apps.system_mgmt.views.operation_log import OperationLogViewSet, CustomOperationLogViewSet
 from apps.system_mgmt.views.user_manage import KeycloakGroupViewSet, KeycloakUserViewSet, KeycloakRoleViewSet
 
 
@@ -18,6 +18,7 @@ router.register(r"role", KeycloakRoleViewSet, basename="role")
 
 router.register(r"menu", MenuManageModelViewSet, basename="menu")
 router.register(r"operation_log", OperationLogViewSet, basename="operation_log")
+router.register(r"operation_log/custom", CustomOperationLogViewSet, basename="operation_log_custom")
 
 urlpatterns = [path("api/", include(router.urls)), ]
 
--- apps/system_mgmt/views/operation_log.py
+++ apps/system_mgmt/views/operation_log.py
@@ -1,8 +1,11 @@
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.decorators import action
 from rest_framework.mixins import ListModelMixin
-from rest_framework.viewsets import GenericViewSet
+from rest_framework.viewsets import GenericViewSet, ViewSet
 
 from apps.core.decorators.uma_permission import uma_permission
 from apps.core.utils.drf_utils import CustomPageNumberPagination
+from apps.core.utils.web_utils import WebUtils
 from apps.system_mgmt.filters.operation_log import OperationLogFilter
 from apps.system_mgmt.models.operation_log import OperationLog
 from apps.system_mgmt.serializers.operation_log import OperationLogSer
@@ -19,3 +22,14 @@ class OperationLogViewSet(ListModelMixin, GenericViewSet):
     @uma_permission("operation_log_list")
     def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
+
+
+class CustomOperationLogViewSet(ViewSet):
+        @swagger_auto_schema(
+            operation_id="operate_type_enum",
+            operation_description="获取操作类型枚举值",
+        )
+        @action(detail=False, methods=["get"], url_path="operate_type/enum")
+        @uma_permission("operate_type_enum")
+        def enum(self, request):
+            return WebUtils.response_success(OperationLog.OPERATE_TYPE_DICT)
