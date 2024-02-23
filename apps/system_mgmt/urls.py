02ebbe964f4795f96d679dd1a6911215ebd62c73
umaru <15875339926@139.com>
2024-02-23 17:00:43 +0800

    调整URL路由

--- apps/system_mgmt/urls.py
+++ apps/system_mgmt/urls.py
@@ -5,22 +5,34 @@ from apps.system_mgmt.views.log_entry_viewset import LogEntryViewSet
 from apps.system_mgmt.views.login_info import LoginInfoView
 from apps.system_mgmt.views.logo import LogoViewSet
 from apps.system_mgmt.views.menu_manage import MenuManageModelViewSet
-from apps.system_mgmt.views.operation_log import OperationLogViewSet, CustomOperationLogViewSet
-from apps.system_mgmt.views.user_manage import KeycloakGroupViewSet, KeycloakUserViewSet, KeycloakRoleViewSet
+from apps.system_mgmt.views.operation_log import (
+    OperationLogViewSet,
+    CustomOperationLogViewSet,
+)
+from apps.system_mgmt.views.user_manage import (
+    KeycloakGroupViewSet,
+    KeycloakUserViewSet,
+    KeycloakRoleViewSet,
+)
 
 
 router = routers.DefaultRouter()
-router.register(r"audit_log", LogEntryViewSet, basename="audit_log")
+router.register(r"api/audit_log", LogEntryViewSet, basename="audit_log")
 
-router.register(r"group", KeycloakGroupViewSet, basename="group")
-router.register(r"user", KeycloakUserViewSet, basename="user")
-router.register(r"role", KeycloakRoleViewSet, basename="role")
+router.register(r"api/group", KeycloakGroupViewSet, basename="group")
+router.register(r"api/user", KeycloakUserViewSet, basename="user")
+router.register(r"api/role", KeycloakRoleViewSet, basename="role")
 
-router.register(r"menu", MenuManageModelViewSet, basename="menu")
-router.register(r"operation_log", OperationLogViewSet, basename="operation_log")
-router.register(r"operation_log/custom", CustomOperationLogViewSet, basename="operation_log_custom")
+router.register(r"api/menu", MenuManageModelViewSet, basename="menu")
+router.register(r"api/operation_log", OperationLogViewSet, basename="operation_log")
+router.register(r"api/operation_log", OperationLogViewSet, basename="operation_log")
+router.register(
+    r"api/operation_log/custom",
+    CustomOperationLogViewSet,
+    basename="operation_log_custom",
+)
 
-urlpatterns = [path("api/", include(router.urls)), ]
+urlpatterns = router.urls
 
 urlpatterns += [
     path(r"api/logo/", LogoViewSet.as_view({"get": "retrieve", "put": "update"})),
