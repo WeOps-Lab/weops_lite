363cd4d1e8e24681847df76e7924a5491a9f5196
baiyf <baiyf@canway.net>
2024-03-08 16:34:43 +0800

    bugfix-修复路由访问错误问题

--- apps/system_mgmt/urls.py
+++ apps/system_mgmt/urls.py
@@ -1,4 +1,4 @@
-from django.urls import include, path
+from django.urls import path
 from rest_framework import routers
 
 from apps.system_mgmt.views.group_manage import KeycloakGroupViewSet
@@ -6,26 +6,32 @@ from apps.system_mgmt.views.log_entry_viewset import LogEntryViewSet
 from apps.system_mgmt.views.login_info import LoginInfoView
 from apps.system_mgmt.views.logo import LogoViewSet
 from apps.system_mgmt.views.menu_manage import MenuManageModelViewSet
+
 from apps.system_mgmt.views.operation_log import OperationLogViewSet, CustomOperationLogViewSet
 from apps.system_mgmt.views.role_manage import KeycloakRoleViewSet
 from apps.system_mgmt.views.user_manage import KeycloakUserViewSet
 
-# 创建路由分组
-api_router = routers.DefaultRouter()
-api_router.register(r"audit_log", LogEntryViewSet, basename="audit_log")
-api_router.register(r"group", KeycloakGroupViewSet, basename="group")
-api_router.register(r"user", KeycloakUserViewSet, basename="user")
-api_router.register(r"role", KeycloakRoleViewSet, basename="role")
-api_router.register(r"menu", MenuManageModelViewSet, basename="menu")
-api_router.register(r"operation_log", OperationLogViewSet, basename="operation_log")
-api_router.register(r"operation_log/custom", CustomOperationLogViewSet, basename="operation_log_custom")
 
-logo_router = routers.DefaultRouter()
-logo_router.register(r"", LogoViewSet, basename="logo")
+router = routers.DefaultRouter()
+router.register(r"api/audit_log", LogEntryViewSet, basename="audit_log")
 
-urlpatterns = [
-    path(r"api/", include(api_router.urls)),
-    path(r"api/logo/", include(logo_router.urls)),
+router.register(r"api/group", KeycloakGroupViewSet, basename="group")
+router.register(r"api/user", KeycloakUserViewSet, basename="user")
+router.register(r"api/role", KeycloakRoleViewSet, basename="role")
+
+router.register(r"api/menu", MenuManageModelViewSet, basename="menu")
+router.register(r"api/operation_log", OperationLogViewSet, basename="operation_log")
+router.register(r"api/operation_log", OperationLogViewSet, basename="operation_log")
+router.register(
+    r"api/operation_log/custom",
+    CustomOperationLogViewSet,
+    basename="operation_log_custom",
+)
+
+urlpatterns = router.urls
+
+urlpatterns += [
+    path(r"api/logo/", LogoViewSet.as_view({"get": "retrieve", "put": "update"})),
     path(r"api/logo/reset/", LogoViewSet.as_view({"post": "reset"})),
     path(r"api/login_info/", LoginInfoView.as_view()),
 ]
