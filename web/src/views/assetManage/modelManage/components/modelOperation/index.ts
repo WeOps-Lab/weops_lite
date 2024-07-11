49114d15c97359934a98847280b84492f3738091
CharlesWhj <1253297146@qq.com>
2024-07-11 18:23:34 +0800

    bugfix: 资产模型管理中的图标更改

--- web/src/views/assetManage/modelManage/components/modelOperation/index.ts
+++ web/src/views/assetManage/modelManage/components/modelOperation/index.ts
@@ -42,7 +42,7 @@ export default class ModelOperation extends Vue {
     formDataV2 = {}
     typeList: Array<any> = [
         { id: 'base', name: '基础模型', desc: '基础模型用于管理主机、数据库等基础资产，可在“资产-资产数据”中管理和查看' },
-        { id: 'credential', name: '凭据模型', desc: '凭据模型用于管理主资产凭据，可在“资产-资产数据”中管理和使用，并且有单独的授权体系' }
+        { id: 'credential', name: '凭据模型', desc: '凭据模型用于管理资产凭据，可在“资产-资产数据”中管理和使用，并且有单独的授权体系' }
     ]
     get isAdd() {
         return this.currentType === 'add'
