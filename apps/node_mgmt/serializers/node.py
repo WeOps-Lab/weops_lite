ce716d35a92a031ce3f82bd5d792a664f2bd16b8
baiyf <baiyf@canway.net>
2024-07-24 19:45:38 +0800

    feature-节点管理

---
+++ apps/node_mgmt/serializers/node.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+
+from apps.node_mgmt.models.node import Node
+
+
+class NodeSerializer(serializers.ModelSerializer):
+    sidecar_status = serializers.CharField(read_only=True)
+    node_ip = serializers.CharField(allow_blank=True)
+
+    class Meta:
+        model = Node
+        fields = ['id', 'model_id', 'node_id', 'node_name', 'node_ip', 'sidecar_status']
